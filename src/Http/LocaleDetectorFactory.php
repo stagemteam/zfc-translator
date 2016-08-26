6bc4b9009ecc8c787e6d30b09be273f0c1f61852
popovsergiy <popow.sergiy@gmail.com>
2016-08-26 12:43:32 +0300

    Factory has been added

---
+++ src/Http/LocaleDetectorFactory.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @category Agere
+ * @package Agere_Translator
+ * @author Popov Sergiy <popov@agere.com.ua>
+ * @datetime: 25.08.2016 12:44
+ */
+namespace Agere\Translator\Http;
+
+class LocaleDetectorFactory
+{
+    public function __invoke($sm)
+    {
+        $config = $sm->get('Config');
+        $detector = new LocaleDetector($config['translator']['locales']);
+        $detector->setDefaultLocale($config['translator']['locale']);
+
+        return $detector;
+    }
+}
